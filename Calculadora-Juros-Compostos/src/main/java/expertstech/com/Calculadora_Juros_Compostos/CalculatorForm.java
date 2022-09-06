e1e9c80ddfc4a253bcf217f316e46c1e3bebae8e
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2022-09-05 21:07:43 -0300

    Creating buttons and panels

--- Calculadora-Juros-Compostos/src/main/java/expertstech/com/Calculadora_Juros_Compostos/CalculatorForm.java
+++ Calculadora-Juros-Compostos/src/main/java/expertstech/com/Calculadora_Juros_Compostos/CalculatorForm.java
@@ -1,8 +1,33 @@
 package expertstech.com.Calculadora_Juros_Compostos;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public abstract class CalculatorForm extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected JPanel pnlForm;
+	protected JPanel pnlRodape;
+	
+	protected JButton btnCalcular;
+	protected JButton btnLimpar;
+	protected JButton btnFechar;	
+
+	protected JLabel lblCapital;
+	protected JTextField txtCapital;
+
+	protected JLabel lblTaxaJuros;
+	protected JTextField txtTaxaJuros;
+
+	protected JLabel lblPeriodo;
+	protected JTextField txtPeriodo;
+
+	protected JLabel lblMontante;
+	protected JTextField txtMontante;
 
 	public CalculatorForm() {
 
